6c92e9e0f97ec8f8eb9a79e45aa48c0e32c19ae8
DESKTOP-MDEFT09\Muhammet Korçil <korcilmuhammet@gmail.com>
2022-01-30 17:50:39 +0300

    commit

---
+++ hackerrankDay2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hackerrankDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
